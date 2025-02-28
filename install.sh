45fd21a61313a55a5455424bb2e3782bbd7ea967
Nix4n <theonetwothe@gmail.com>
2025-02-28 23:05:18 +0400

    list you need install on host and in emacs

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 mkdir ~/.emacs.backups
 cp emacs ~/.emacs
+
+# you also need install: llvm, llvm-libs, clang
+# in emacs: irony, company, company-irony, company-irony-c-headers,
+# [company-clang]

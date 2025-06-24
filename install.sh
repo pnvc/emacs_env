ddbb3daeaefaa0eabbcd7f3a1cc73da58b9bacce
nixan <theonetwothe@gmail.com>
2025-06-24 22:13:31 +0400

    continue last commit

--- install.sh
+++ install.sh
@@ -2,7 +2,3 @@
 
 mkdir ~/.emacs.backups
 cp emacs ~/.emacs
-
-# you also need install: llvm, llvm-libs, clang
-# in emacs: irony, company, company-irony, company-irony-c-headers,
-# [company-clang]
